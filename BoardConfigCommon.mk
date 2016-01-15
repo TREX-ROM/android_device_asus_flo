d4ed08091ed61c95a9d9a30f425fed5d86f95b14
t-rex600 <fisch890@gmail.com>
2016-01-15 16:10:37 +0100

    liquify

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,6 +36,9 @@ TARGET_KERNEL_SOURCE := kernel/google/msm
 TARGET_KERNEL_CONFIG := cyanogen_flo_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
+# Changelog
+LIQUID_CHANGELOG := true
+
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
 BOARD_USES_FLUENCE_INCALL := true
